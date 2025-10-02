a698f2d1e5cf231407e21dbe774fa8037ab776b6
Angelo <martinezglielmo@gmail.com>
2025-10-02 17:09:44 +0100

    made create and findAll in the controller

--- backend/controllers/song.controller.js
+++ backend/controllers/song.controller.js
@@ -2,8 +2,38 @@ const db = require("./models");
 const Song = db.song;
 const Op = db.Sequelize.Op;
 
-exports.create = (req, res) => {};
-exports.findAll = (req, res) => {};
+exports.create = (req, res) => {
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Song must have a title",
+    });
+    return;
+  }
+  const song = {
+    title: req.body.title,
+    artist: req.body.artist,
+  };
+  Song.create(song)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while adding the song",
+      });
+    });
+};
+exports.findAll = (req, res) => {
+  Song.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error ocurred while retrieving the songs",
+      });
+    });
+};
 exports.findOne = (req, res) => {};
 exports.update = (req, res) => {};
 exports.delete = (req, res) => {};
--- backend/models/song.model.js
+++ backend/models/song.model.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, Sequelize) => {
     title: {
       type: Sequelize.STRING,
     },
-    author: {
+    artist: {
       type: Sequelize.STRING,
     },
   });
